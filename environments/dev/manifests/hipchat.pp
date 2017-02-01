ab90b9403fd5f434e7122d089e52fa8fe7e03506
Patrick Double <pat@patdouble.com>
2017-02-01 08:29:22 -0600

    Hipchat 4

---
+++ environments/dev/manifests/hipchat.pp
@@ -0,0 +1,16 @@
+class hipchat {
+  file { '/etc/yum.repos.d/hipchat.repo':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => '[atlassian-hipchat]
+name=Atlassian Hipchat
+baseurl=https://atlassian.artifactoryonline.com/atlassian/hipchat-yum-client/
+enabled=1
+gpgcheck=0
+#gpgkey=https://www.hipchat.com/keys/hipchat-linux.key
+',
+  }->
+  package { 'hipchat4': }
+}
