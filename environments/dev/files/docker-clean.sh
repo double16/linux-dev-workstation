646a153921a9844aa7cdbcbe4928fd90d1f474bf
Patrick Double <pat@patdouble.com>
2017-04-14 09:31:31 -0500

    Fix docker-clean to not run docker command if there are no arguments

--- environments/dev/files/docker-clean.sh
+++ environments/dev/files/docker-clean.sh
@@ -3,6 +3,6 @@
 # 1. Forcibly remove containers
 # 2. Remove volumes
 # 3. Remove untagged images (generally temporary or old builds)
-docker ps -qa | xargs docker rm -f
-docker volume ls -f dangling=true -q | xargs docker volume rm
-docker images -f dangling=true -q | xargs docker rmi
+docker ps -qa | xargs -r docker rm -f
+docker volume ls -f dangling=true -q | xargs -r docker volume rm
+docker images -f dangling=true -q | xargs -r docker rmi
