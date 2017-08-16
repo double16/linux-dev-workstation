a3cf493f04dd4e727ae5348151908ae250d0fd2c
Patrick Double <pat@patdouble.com>
2017-08-16 15:32:16 -0500

    Kitematic 0.17.1

--- environments/dev/manifests/kitematic.pp
+++ environments/dev/manifests/kitematic.pp
@@ -1,9 +1,9 @@
 class kitematic
 {
   package { ['rpmrebuild','zsh','libnotify']: }
-  ->archive { 'Kitematic-0.17.0':
+  ->archive { 'Kitematic-0.17.1':
     ensure           => present,
-    url              => 'https://github.com/docker/kitematic/releases/download/v0.17.0/Kitematic-0.17.0-Ubuntu.zip',
+    url              => 'https://github.com/docker/kitematic/releases/download/v0.17.1/Kitematic-0.17.1-Ubuntu.zip',
     target           => '/opt',
     follow_redirects => true,
     extension        => 'zip',
@@ -11,13 +11,13 @@ class kitematic
     #checksum_type => 'sha256',
   }
   ->exec { 'Kitematic rpm':
-    command => '/usr/bin/alien -r -k /opt/Kitematic_0.17.0_amd64.deb',
-    creates => '/opt/kitematic-0.17.0-1.x86_64.rpm',
+    command => '/usr/bin/alien -r -k /opt/Kitematic_0.17.1_amd64.deb',
+    creates => '/opt/kitematic-0.17.1-1.x86_64.rpm',
     cwd     => '/opt',
     require => Package['alien'],
   }
   ->exec { 'Kitematic rpm fixes':
-    command => '/usr/bin/rpmrebuild --batch --install --change-spec-files=\'grep -v "\\"/\\"\\|\\"/usr\\"\\|\\"/usr/bin\\"\\|\\"/usr/share\\"\\|\\"/usr/share/applications\\"\\|\\"/usr/share/doc\\"\\|\\"/usr/share/pixmaps\\""\' -p /opt/kitematic-0.17.0-1.x86_64.rpm',
+    command => '/usr/bin/rpmrebuild --batch --install --change-spec-files=\'grep -v "\\"/\\"\\|\\"/usr\\"\\|\\"/usr/bin\\"\\|\\"/usr/share\\"\\|\\"/usr/share/applications\\"\\|\\"/usr/share/doc\\"\\|\\"/usr/share/pixmaps\\""\' -p /opt/kitematic-0.17.1-1.x86_64.rpm',
     creates => '/usr/bin/kitematic',
     require => Package['rpmrebuild'],
   }
