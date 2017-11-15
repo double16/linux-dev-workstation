c3d185511dce9e731739240c2009853b288d29ad
Patrick Double <pat@patdouble.com>
2017-11-15 15:12:38 -0600

    Change RPM installs to use archive instead of remote_file to prevent unnecessary downloads

--- environments/dev/modules/private/manifests/rlang.pp
+++ environments/dev/modules/private/manifests/rlang.pp
@@ -2,19 +2,17 @@
 # Install R language things
 #
 class private::rlang {
-  package { 'R': }
-
-  package { ['gstreamer', 'gstreamer-plugins-base' ]: }
-  ->remote_file { '/tmp/vagrant-cache/rstudio-1.1.383-x86_64.rpm':
-    ensure        => present,
-    source        => 'https://download1.rstudio.org/rstudio-1.1.383-x86_64.rpm',
-    checksum      => 'ae400e2504ec9c5862343c24fe3cd61d',
-    checksum_type => 'md5',
-    require       => File['/tmp/vagrant-cache'],
-  }
-  ->package { 'rstudio':
-    source   => '/tmp/vagrant-cache/rstudio-1.1.383-x86_64.rpm',
-    provider => 'rpm',
+  package { [ 'R', 'gstreamer', 'gstreamer-plugins-base' ]: }
+  ->archive { '/tmp/vagrant-cache/rstudio-1.1.383-x86_64.rpm':
+    ensure          => present,
+    source          => 'https://download1.rstudio.org/rstudio-1.1.383-x86_64.rpm',
+    extract         => true,
+    extract_path    => '/tmp',
+    extract_command => 'rpm -ivh %s',
+    creates         => '/usr/bin/rstudio',
+    checksum        => 'ae400e2504ec9c5862343c24fe3cd61d',
+    checksum_type   => 'md5',
+    require         => File['/tmp/vagrant-cache'],
   }
 }
 
--- environments/dev/modules/private/manifests/slack.pp
+++ environments/dev/modules/private/manifests/slack.pp
@@ -1,16 +1,15 @@
 class private::slack {
   package { ['libappindicator', 'libsecret']: }
-  remote_file { '/tmp/vagrant-cache/slack-2.8.2-0.1.fc21.x86_64.rpm':
-    ensure        => present,
-    source        => 'https://downloads.slack-edge.com/linux_releases/slack-2.8.2-0.1.fc21.x86_64.rpm',
-    checksum      => '6fc2c41e650e8b29904bc832a499bb3ac2480d216f2003d4c9581ee222e7356c',
-    checksum_type => 'sha256',
-    require       => File['/tmp/vagrant-cache'],
-  }
-  ->package { 'slack':
-    provider => rpm,
-    source   => '/tmp/vagrant-cache/slack-2.8.2-0.1.fc21.x86_64.rpm',
-    require  => Package['libappindicator'],
+  ->archive { '/tmp/vagrant-cache/slack-2.8.2-0.1.fc21.x86_64.rpm':
+    ensure          => present,
+    source          => 'https://downloads.slack-edge.com/linux_releases/slack-2.8.2-0.1.fc21.x86_64.rpm',
+    extract         => true,
+    extract_path    => '/tmp',
+    extract_command => 'rpm -ivh %s',
+    creates         => '/usr/bin/slack',
+    checksum        => '6fc2c41e650e8b29904bc832a499bb3ac2480d216f2003d4c9581ee222e7356c',
+    checksum_type   => 'sha256',
+    require         => File['/tmp/vagrant-cache'],
   }
 }
 
