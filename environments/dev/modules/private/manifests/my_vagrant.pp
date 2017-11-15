4ac48670a9bb3feb913f338a3537e92fbe3ba38b
Patrick Double <pat@patdouble.com>
2017-11-15 10:36:49 -0600

    vagrant 2.0.1

--- environments/dev/modules/private/manifests/my_vagrant.pp
+++ environments/dev/modules/private/manifests/my_vagrant.pp
@@ -1,7 +1,7 @@
 class private::my_vagrant {
   class { 'vagrant':
     ensure  => present,
-    version => '2.0.0',
+    version => '2.0.1',
   }
   vagrant::plugin { 'vagrant-vbguest':
     user => 'vagrant',
