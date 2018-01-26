590764798b18bb899bcfac8a549460d42047563d
Patrick Double <pat@patdouble.com>
2018-01-26 15:42:41 -0600

    Fixes for Triton and CentOS 1801 AMI

--- environments/dev/modules/private/manifests/triton.pp
+++ environments/dev/modules/private/manifests/triton.pp
@@ -1,13 +1,17 @@
 # Joyent Triton CLI
 # From https://docs.joyent.com/public-cloud/api-access/cloudapi
 class private::triton {
-  nodenv::package { 'triton on LTS':
+  $triton_node_version = '6.11.5'
+
+  nodenv::build { $triton_node_version: }
+
+  nodenv::package { 'triton':
     package      => 'triton',
-    version      => '5.4.0',
-    node_version => $::private::my_node::node_lts,
+    version      => '5.5.0',
+    node_version => $triton_node_version,
   }
   ->exec {'triton completion':
-    command     => 'nodenv rehash && triton completion > /etc/bash_completion.d/triton',
+    command     => "/opt/nodenv/versions/${triton_node_version}/bin/triton completion > /etc/bash_completion.d/triton",
     creates     => '/etc/bash_completion.d/triton',
     path        => ['/bin','/usr/bin','/opt/nodenv/bin','/opt/nodenv/shims'],
     environment => ['HOME=/root'],
