ab5ff24d377970d1ec2cd4cf6a9c5c7cf17d15fb
Patrick Double <pat@patdouble.com>
2018-07-31 11:44:38 -0500

    More vagrant plugins

--- environments/dev/modules/private/manifests/my_vagrant.pp
+++ environments/dev/modules/private/manifests/my_vagrant.pp
@@ -4,14 +4,37 @@
 class private::my_vagrant {
   $version = lookup('vagrant', Hash)['version']
 
+  package { [ 'fuse-sshfs' ]: }
+
   class { 'vagrant':
     ensure  => present,
     version => $version,
   }
-  vagrant::plugin { 'vagrant-vbguest':
+  vagrant::plugin { [
+    'vagrant-cachier',
+    'vagrant-aws',
+    'vagrant-azure',
+    'vagrant-google',
+    'vagrant-joyent',
+    'vagrant-sshfs',
+    ]:
     user => 'vagrant',
   }
-  vagrant::plugin { 'vagrant-cachier':
-    user => 'vagrant',
+
+unless $::virtual == 'docker' or $::virtual =~ /xen.*/ {
+    vagrant::plugin { [
+      'vagrant-vbguest',
+      'vagrant-disksize',
+      ]:
+      user => 'vagrant',
+    }
+    package { [
+      'libvirt',
+      'libvirt-devel',
+      ]:
+    }
+    ->vagrant::plugin { 'vagrant-libvirt': 
+      user => 'vagrant',
+    }
   }
 }
