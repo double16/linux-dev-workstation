eea0375085200c49fb455ffa10ebaafc972567e7
Patrick Double <pat@patdouble.com>
2018-08-16 12:48:46 -0500

    give node-gyp more time

--- environments/dev/modules/private/manifests/triton.pp
+++ environments/dev/modules/private/manifests/triton.pp
@@ -10,7 +10,7 @@ class private::triton {
     version      => '6.0.0',
     node_version => $triton_node_version,
     # node-gyp takes a long time, it is compiling the native bindings for node
-    timeout      => 3600,
+    timeout      => 7200,
   }
   ->exec {'triton completion':
     command     => "/opt/nodenv/versions/${triton_node_version}/bin/triton completion > /etc/bash_completion.d/triton",
