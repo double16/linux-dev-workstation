2943355c3243bb6eb134c8fe2f4c8a1409272e79
Patrick Double <pat@patdouble.com>
2018-08-21 15:11:45 -0500

    Change ownership of /opt/puppetlabs to vagrant

---
+++ script/post-perm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eux
+
+SSH_USER=${SSH_USERNAME:-vagrant}
+echo "==> Configuring permissions for ${SSH_USER}"
+
+[ -d "/opt/puppetlabs" ] && chown -R "${SSH_USER}:${SSH_USER}" /opt/puppetlabs
