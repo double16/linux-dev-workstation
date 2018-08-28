40491156b9d3f5472274da5369cdcab39afb7005
Patrick Double <pat@patdouble.com>
2018-08-28 05:54:13 -0500

    Version refresh, vim repo change

--- environments/dev/modules/private/manifests/my_vim.pp
+++ environments/dev/modules/private/manifests/my_vim.pp
@@ -1,45 +1,19 @@
 class private::my_vim {
   $global_color_scheme = pick($::theme, lookup('theme::default'))
 
-  yum::gpgkey { '/etc/pki/rpm-gpg/RPM-GPG-KEY-mcepl-vim8-epel7':
-    ensure  => present,
-    content => '-----BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v2
-
-mQENBFfac9gBCAC8K6Ps7dW/wdbWLp99NsXi4MuZ57ktabj2dzaDfzArr47/yt81
-z8Yriw9n9ZjTo9aZAtyMhGKAi2Eo4j9IKoGMy8xbbHRAUUdL13gJ4GfrdRqGyVF/
-+cLseLwk9mam6hPVgx2BAcaA/6jaMM6wcIBrVE57l+V67QbMccUt9EOF2RXjiulL
-oF1ylqkGhfhvZtw+pCCtxIwbfh21FlXBAmr+yXG0JRYB4ezEkqEcr+yFMNEKt+vl
-LNEoCh3psgDgDZvs4V5PVyivIaVR4XEcpkbRSiO4iEKZREeZ4xsaygsoPkh1lrWp
-k+BGB79Y4tvG2FlP7sklPZrFf9+7k3sAJguRABEBAAG0NG1jZXBsX3ZpbTggKE5v
-bmUpIDxtY2VwbCN2aW04QGNvcHIuZmVkb3JhaG9zdGVkLm9yZz6JAT0EEwEIACcF
-Alfac9gCGy8FCQlmAYAFCwkIBwIGFQgJCgsCBBYCAwECHgECF4AACgkQPtoan0Hw
-2KiwGQgAtarc4zvIoolNp4QbRxG+dH/n9Cq+/wh1Fwh6q4/zTFDWllDc2KPe+0d+
-UpXjeF/rb6AlDgY47n7rMzr1TSq3Vn+rZBlKOLJIS/vdDNVgQJxB/xwpJ1UQaxHG
-nhr6hxI3NDtl1Rpre2R8020VGBfnuKbbtpEyU/jfCx7XKT/jjydzovLBh2PDykvu
-QkyKHtsuhgROJdj/cDi1e7g0JWDhm4r+79OeT/q7Azf4AqUzfeEwOVpS0FvqOdK0
-uZqdSXy8bHM0xVSJSC0qhr9HJi9r13Eb+1NCKNzZqe/FqOUTWM5e5OHqb3As6Cjy
-EM2mrdKYTJ+wFGIm+bpFqzRpoQbi8g==
-=gdmo
------END PGP PUBLIC KEY BLOCK-----
-',
+  package { 'gf-release':
+    source   => 'http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm',
+    provider => 'rpm',
   }
-  ->remote_file {'/etc/yum.repos.d/mcepl-vim8-epel-7.repo':
-    ensure => present,
-    source => 'https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo',
-    mode   => '0644',
-    owner  => 0,
-    group  => 0,
-  }
-  ->Package<| |>
-  Remote_file['/etc/yum.repos.d/mcepl-vim8-epel-7.repo']
+  ->Package<| title != 'gf-release' |>
+  Package['gf-release']
   ->Yum::Group<| |>
 
   # In some cases replacing vim-minimal will remove 'sudo', but instaling vim-enhanced on top of vim-minimal is allowed
   exec { 'yum install -y vim-enhanced || yum replace -y vim-minimal --replace-with=vim-enhanced':
     unless  => 'yum --cacheonly list installed vim-enhanced | grep -q 8.0',
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-    require => [ Yum::Plugin['replace'], Remote_file['/etc/yum.repos.d/mcepl-vim8-epel-7.repo'] ],
+    require => [ Yum::Plugin['replace'], Package['gf-release'] ],
   }
   ->class { '::vim':
     autoupgrade    => true,
