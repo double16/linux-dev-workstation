28e17daf6446420ddef220e151ccc6d3c910e229
Patrick Double <pat@patdouble.com>
2018-12-08 18:18:01 -0600

    Include minikube with VM+docker-ce

--- environments/dev/modules/private/manifests/my_docker.pp
+++ environments/dev/modules/private/manifests/my_docker.pp
@@ -87,6 +87,8 @@ class private::my_docker {
         docker_users                => [ 'vagrant' ],
         service_overrides_template  => 'private/docker-service-overrides.erb',
       }
+
+      include private::minikube
     }
 
     cron { 'docker-prune':
@@ -148,50 +150,7 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
     }
     ->package { 'kubectl': }
 
-    $minikube_config = lookup('minikube', Hash)
-    $minikube_version = $minikube_config['version']
-    $minikube_checksum = $minikube_config['checksum']
-    private::cached_remote_file { '/usr/bin/minikube':
-      cache_name    => "minikube-${minikube_version}",
-      source        => "https://storage.googleapis.com/minikube/releases/v${minikube_version}/minikube-linux-amd64",
-      # 2018-05-01 storage.googleapis.com uses a cross-signed TLS cert, current Ruby/Net::HTTP does not recognize it
-      verify_peer   => false,
-      owner         => 0,
-      group         => 'root',
-      mode          => '0755',
-      checksum      => $minikube_checksum,
-      checksum_type => 'sha256',
-    }
-
-    file_line { 'MINIKUBE_VM_DRIVER':
-      path  => '/etc/environment',
-      line  => 'MINIKUBE_VM_DRIVER=none',
-      match => '^MINIKUBE_VM_DRIVER=',
-    }
-
-    file_line { 'MINIKUBE_BOOTSTRAPPER':
-      path  => '/etc/environment',
-      line  => 'MINIKUBE_BOOTSTRAPPER=localkube',
-      match => '^MINIKUBE_BOOTSTRAPPER=',
-    }
-
-    file_line { 'MINIKUBE_SHOWBOOTSTRAPPERDEPRECATIONNOTIFICATION':
-      path  => '/etc/environment',
-      line  => 'MINIKUBE_SHOWBOOTSTRAPPERDEPRECATIONNOTIFICATION=false',
-      match => '^MINIKUBE_SHOWBOOTSTRAPPERDEPRECATIONNOTIFICATION=',
-    }
-
-    file_line { 'CHANGE_MINIKUBE_NONE_USER':
-      path  => '/etc/environment',
-      line  => 'CHANGE_MINIKUBE_NONE_USER=true',
-      match => '^CHANGE_MINIKUBE_NONE_USER=',
-    }
-
-    file_line { 'MINIKUBE_WANTNONEDRIVERWARNING':
-      path  => '/etc/environment',
-      line  => 'MINIKUBE_WANTNONEDRIVERWARNING=false',
-      match => '^MINIKUBE_WANTNONEDRIVERWARNING=',
-    }
+    include private::minikube
   }
 
   package { 'docker-compose':
