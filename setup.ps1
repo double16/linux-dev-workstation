3b3f7afb914e7fcc6a377dde0c1489d6d5918179
Patrick Double <pat@patdouble.com>
2019-10-15 12:10:31 -0500

    Fix setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,13 +1,15 @@
 # Install software necessary for running a Vagrant development environment
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$ChocoInstalled = Test-Path -Path "$env:ProgramData\Chocolatey"
+if ($ChocoInstalled -eq $false) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
-choco install git
-choco install vagrant
-choco install ruby
-choco install vnc-viewer
-choco install docker-desktop
-choco install virtualbox
-choco install packer
-choco install vscode
-
+choco install -y git
+choco install -y vagrant
+choco install -y ruby
+choco install -y vnc-viewer
+choco install -y docker-desktop
+choco install -y virtualbox
+choco install -y packer
+choco install -y vscode vscode-gitignore vscode-vsliveshare vscode-puppet vscode-ruby vscode-yaml vscode-gitlens vscode-markdownlint vscode-docker vscode-powershell vscode-settingssync vscode-icons
